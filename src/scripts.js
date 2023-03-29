c1d7b666376a422a1319854a588bbc5b884105f1
Kamilla <kamillakkngmail.com>
2023-03-29 12:08:21 +0200

    cookies

--- src/scripts.js
+++ src/scripts.js
@@ -36,3 +36,19 @@ for (i = 0; i < info.length; i++) {
     }
   });
 }
+
+const cookieBox = document.querySelector('.wrapper'),
+  acceptBtn = cookieBox.querySelector('.buttons button');
+
+acceptBtn.onclick = () => {
+  document.cookie = 'CookieBy=Vkive; max-age=' + 60 * 60 * 24 * 30;
+  if (document.cookie) {
+    cookieBox.classList.add('hide');
+  } else {
+    alert("Cookie can't be set!");
+  }
+};
+let checkCookie = document.cookie.indexOf('CookieBy=Vkive');
+checkCookie != -1
+  ? cookieBox.classList.add('hide')
+  : cookieBox.classList.remove('hide');
