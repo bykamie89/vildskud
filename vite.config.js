dc4f29f41e2dd1a124d3c436939486e43c9e7cf5
“Mia <mb.mia.bosse@gmail.com>
2023-03-30 08:49:30 +0200

    footer sider lagt ind i vite

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
 // vite.config.js
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: resolve(__dirname, 'src'),
-  base: './',
+  root: resolve(__dirname, "src"),
+  base: "./",
   preview: {
     host: true,
   },
-  publicDir: resolve('public'),
+  publicDir: resolve("public"),
   build: {
-    outDir: resolve('dist'),
+    outDir: resolve("dist"),
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html'),
-        lokationer: resolve(__dirname, 'src/lokationer.html'),
-        om: resolve(__dirname, 'src/om.html'),
-        billetter: resolve(__dirname, 'src/billetter.html'),
+        main: resolve(__dirname, "src/index.html"),
+        lokationer: resolve(__dirname, "src/lokationer.html"),
+        om: resolve(__dirname, "src/om.html"),
+        billetter: resolve(__dirname, "src/billetter.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        privatlivspolitik: resolve(__dirname, "src/privatlivspolitik.html"),
+        cookiepolitik: resolve(__dirname, "src/cookiepolitik.html"),
+        samarbejdspartnere: resolve(__dirname, "src/samarbejdspartnere.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
